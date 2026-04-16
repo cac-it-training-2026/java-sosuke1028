a0e57782f4a9e2ff43157a7872900596b9ce8eae
marutaso <marutaso@C010423582.zipangu.cac.co.jp>
2026-04-16 16:52:33 +0900

    6-4

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -7,41 +7,25 @@ import java.io.InputStreamReader;
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-         int ammountBooks = 0;
-         int SellFlag = 0;
-         int bookprice = 200;
-         
-         System.out.println("本を売却します。");
+		int ammountBooks = 0;
+		int SellFlag = 0;
+		int bookprice = 200;
 
-         
-         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-         String str = reader.readLine();
- 		int sellFlag = Integer.parseInt(str);
- 		
- 		
- 		System.out.println("本を売却します。");
-		System.out.println("はい：0、いいえ：１");
-        
+		System.out.println("本を売却します。");
+
 		do {
 			ammountBooks++;
-			
+
 			System.out.println("本を売却する数を一つ増やしますか？");
 			System.out.println("はい：0、いいえ：１");
-			
-			
-		}
-		
-         while(sellFlag==0) {
-        	 ammountBooks++;
-        	 
-        	 
-        	 
-        	 
-        	 
-        	 
-         }
-         
-		
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String str = reader.readLine();
+			SellFlag = Integer.parseInt(str);
+
+		} while (SellFlag == 0);
+		System.out.println("売却する本の冊数は" + ammountBooks + "冊です。");
+		System.out.println("売却した本の合計金額は" + ammountBooks * bookprice + "円です。");
 
 	}
 
